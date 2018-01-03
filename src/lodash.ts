e6ef29af9b5cd410cbaecb2de67eb2032b05111e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-03 10:54:57 -0800

    fix flatMap

--- src/lodash.ts
+++ src/lodash.ts
@@ -3,7 +3,7 @@ import lodash = require('lodash')
 export default {
   get ary(): typeof lodash.ary { return fetch('lodash/ary') },
   get compact(): typeof lodash.compact { return fetch('lodash/compact') },
-  get flatMap(): typeof lodash.ary { return fetch('lodash/flatMap') },
+  get flatMap(): typeof lodash.flatMap { return fetch('lodash/flatMap') },
   get get(): typeof lodash.get { return fetch('lodash/get') },
   get identity(): typeof lodash.identity { return fetch('lodash/identity') },
   get isEqual(): typeof lodash.isEqual { return fetch('lodash/isEqual') },
